96d0672852b779b82449de975b9691e26a942368
slavos1 <1396260+slavos1@users.noreply.github.com>
2023-07-19 18:40:58 +1000

    Fix version tag as it contains v prefix

--- src/VersionInfo.jsx
+++ src/VersionInfo.jsx
@@ -2,17 +2,17 @@ import { Box, Link, Typography } from "@mui/material";
 import meta from "../package.json";
 
 const VersionInfo = () => {
-  const tag = meta.version;
+  const tag = `v${meta.version}`;
 
   return (
     <Box sx={{ float: "right" }}>
       <Typography variant="body2">
         <Link
-          href={`https://github.com/slavos1/code-font-gallery/tree/${tag}`}
+          href={`https://github.com/slavos1/code-font-gallery/releases/tag/${tag}`}
           rel="noopener noreferrer nofollow noindex"
           target="_blank"
         >
-          {meta.name} v{tag}
+          {meta.name} {tag}
         </Link>
       </Typography>
     </Box>
